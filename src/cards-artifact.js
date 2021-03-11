7c3dee404050df3404e4ec42403d1f28489c1b28
cathrynploehn <ploehn.cathryn@gmail.com>
2021-03-11 17:16:44 -0600

    pdf mvp

--- src/cards-artifact.js
+++ src/cards-artifact.js
@@ -1,31 +1,167 @@
 import p5 from 'p5';
+const PDFDocument = require('pdfkit');
+var blobStream = require('blob-stream');
+import * as Tree from "./tree.js";
 
-function Artifact(basket){
+
+function Artifact(basket, biomes){
 
   var canvasID;
+  var display = true;
 
   var artifactSketch =  async function (p){
+    var radialIncrement = (Math.PI * 2) / 8;
+    var radius;
+    var currentNumBiomes = 0;
+    var tree;
+
     p.setup = async function() {
       p.frameRate(24);
-      var elem = document.getElementById("card-overlay");
-      var canvas = p.createCanvas(elem.clientWidth, elem.clientHeight);
+      p.textSize(12);
+
+      var elem = document.getElementById("artifact-container");
+      var dimensions = elem.clientWidth > 600? 600: elem.clientWidth;
+      var canvas = p.createCanvas(dimensions+50, dimensions+50);
+
+      tree = new Tree.Tree(p);
       canvasID = canvas.id('artifactCanvas')
-      console.log(p)
+      radius = (p.width - 200)/2;
     }
 
     p.draw = function() {
+      if(display){
+        p.background(255, 251, 235)
+        p.stroke(0)
+        p.noFill();
+        p.ellipse(p.width/2, p.height/2, radius*2 - 50)
 
-      p.background(0)
+        var i = Math.PI * 1.5;
+        var index = 1;
+        for(var biome in biomes){
+          var x = (radius * cos(i)) + p.width/2;
+          var y = (radius * sin(i)) + p.height/2;
+
+          if(index > currentNumBiomes){
+            currentNumBiomes = index;
+            tree.genLeaves(radius, i)
+          }
+
+          tree.grow()
+          tree.show()
+
+          index++;
+
+          p.noStroke();
+          p.rectMode(CENTER);
+          p.fill(255, 251, 235)
+          p.rect(p.width/2, p.height/2, 180, 50)
+
+
+          p.textAlign(CENTER, CENTER);
+          p.fill(0)
+          p.push();
+            p.translate(x, y)
+            p.rotate(i - (Math.PI * 1.5));
+            p.text(biome, 0, 0);
+          p.pop();
+          var displayText = "You've collected cards \nfrom " + Object.keys(biomes).length + " realm(s)."
+          p.text(displayText, p.width/2, p.height/2);
+
+          i += radialIncrement;
+        }
+      }
     }
 
   }
+  this.show = () => {
+    display = true;
+  }
+  this.hide = () => {
+    display = false;
+  }
 
-  this.exportCards = () => {
+  function downloadImage(url) {
+    return new Promise((resolve, reject) => {
+      let imageURL = url;
+      var downloadedImg = new Image;
+      downloadedImg.crossOrigin = "Anonymous";
+      downloadedImg.src = imageURL;
+      downloadedImg.addEventListener("load", () => {
+        resolve(downloadedImg)
+      }, false);
+    })
+  }
 
+  this.exportCards = async () => {
+    const doc = new PDFDocument;
+    var stream = doc.pipe(blobStream());
+    var forPDF = document.getElementById('forPDF');
+    var ctx = forPDF.getContext('2d');
+
+    var font = "../assets/Poppins-Regular.ttf"
+    doc.font("Helvetica");
+    // doc.font(font);
+
+    doc.rect(0, 0, 612, 792).fill("#FFFBEB");
+    doc.moveTo(0, 0);
+    doc.fillColor("#000000")
+
+    // cover
+    doc.fontSize(42).text('wildcrafting', {width: 612, align: 'left', x: 0})
+    doc.moveDown()
+    doc.fontSize(30).text("The cards you collected from the exhibit that underpin MozFest 2021.")
+    doc.moveDown()
+    doc.fontSize(12).text(" A project by Ulu Mills, Devika Singh, Cathryn Ploehn, and Jessica Liu.")
+    doc.moveDown();
+    doc.moveDown();
     var canvas = document.getElementById("artifactCanvas");
-    var img = canvas.toDataURL("image/png");
+    var image = canvas.toDataURL("image/png");
+    var x = (612 - 400) / 2;
+    var y = (792 - 400) / 2;
+    doc.image(new Buffer.from(image.replace('data:image/png;base64,',''), 'base64'), {fit: [400, 400], align: 'center', valign: 'center', x: x}); // this will decode your base64 to a new buffer
 
-    document.write('<img src="'+img+'"/>');
+    for(var i = 0; i < basket.length; i++){
+      if(i % 4 == 0) {
+        doc.addPage();
+        doc.rect(0, 0, 612, 792).fill("#FFFBEB");
+        doc.moveTo(0, 0);
+        doc.fillColor("#000000")
+      }
+
+      // Once again we can't use images from iNat because of their CORS policy
+      // var tag = "photo" + (i + 1);
+      // var imgElem = document.getElementById(tag)
+      // var imgElem = downloadImage(basket[i]["observation_photo_url"])
+      // forPDF.height = imgElem.naturalHeight;
+      // forPDF.width = imgElem.naturalWidth;
+      // ctx.drawImage(imgElem, 0, 0);
+      // var uri = forPDF.toDataURL('image/png')
+      // doc.image(new Buffer(uri.replace('data:image/png;base64,',''), 'base64'), {fit: [100, 100], align: 'center', valign: 'center'});
+      // doc.moveDown();
+
+      // Add text
+      var text = basket[i]["participant_name"] + " told us of " + basket[i]["preferred_common_name"];
+      if(basket[i]["description"]) {
+        text += ", saying: " + basket[i]["description"];
+      }
+      var textX = (612 - 400 )/ 2;
+      doc.text(`${text}`, {
+        width: 400,
+        align: 'center',
+        x: textX
+      });
+      doc.moveDown();
+    }
+
+    // finalize the PDF and end the stream
+    doc.end();
+    stream.on('finish', function() {
+      const url = stream.toBlobURL('application/pdf');
+      var a = document.createElement("a");
+      a.href = url;
+      a.download = "MMMMMMMmozFest.pdf";
+      a.click();
+    });
 
   };
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 var observations = [];
 var basket = [];
-var biomes = {};
+// var biomes = {"one biome", "two biome", "three biome"};
+var biomes = {
+
+};
 
 import * as entities from './entities.js';
 import * as search from './search.js';
@@ -11,7 +14,7 @@ var drawArches = () => {
   var sceneEl = document.querySelector('a-scene');
   var numArch = 10;
   var rotationLvl = (Math.PI * 2) / numArch;
-
+  //
   for(var i = 0; i < numArch; i++){
     var d = [];
     for(var j = i; j < observations.length; j += numArch){
@@ -24,19 +27,19 @@ var drawArches = () => {
     sceneEl.appendChild(entityEl);
   }
 
-  var trelisEl = document.createElement('a-entity');
-  trelisEl.setAttribute('trelis', {numArch: 13});
-  sceneEl.appendChild(trelisEl);
+  // var trelisEl = document.createElement('a-entity');
+  // trelisEl.setAttribute('trelis', {numArch: 13});
+  // sceneEl.appendChild(trelisEl);
 
 };
 
 
 $( document ).ready(function() {
+  entities.entities(observations, basket, biomes);
   openClose.splashScreen();
 
   var getData = search.airtableSearch(observations);
   getData.then((d) => {
-    entities.entities(observations, basket);
     drawArches();
   });
 
