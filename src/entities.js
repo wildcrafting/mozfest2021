e73e0befa764d1008e99a9a5b719d019ba6bee7a
Ulu-home <aloha@ulumills.com>
2021-03-14 22:08:58 -1000

    and another

--- src/entities.js
+++ src/entities.js
@@ -50,11 +50,11 @@ var entities = (observations, basket, biomes) => {
         // this.el.object3D.rotation.y ;
         el.addEventListener('mouseenter', function () {
           el.object3D.scale.copy({x: 1.5, y: 1.5, z:1.5});
-          $("#backup-sky").fadeIn(200);
+          $("#backup-sky").fadeTo(200, 1);
         });
         el.addEventListener('mouseleave', function () {
           el.object3D.scale.copy({x: 1, y: 1, z:1});
-          $("#backup-sky").fadeOut(500);
+          $("#backup-sky").fadeTo(500, 0);
         });
         this.el.addEventListener('click', function () {
           el.setAttribute('animation', 'property: position; to: 0 0 0');
